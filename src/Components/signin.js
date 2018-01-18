e3d7dcc4729ceb17c0930714cb4e5278e9a620f2
Tyler Long <tyler@Tylers-MacBook-Pro.local>
2018-01-17 19:46:19 -0500

    new branch

--- src/Components/signin.js
+++ src/Components/signin.js
@@ -47,7 +47,6 @@ export default class Signin extends Component {
             <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
                 <header className="App-header" style={{color: 'white', backgroundColor: 'dodgerBlue', display: 'flex', flexDirection: 'column'}}>
                     <NavbarLambda />
-                    <p>HEY</p>
                 </header>
                 <div style={{backgroundColor: 'dodgerBlue', display: 'flex', justifyContent: 'center'}}>
                     <h2 style={{color: 'white', paddingRight: 20}}><i>Sign In:</i></h2>
