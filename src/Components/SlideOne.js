d1a626b6c710bbcc85337381ec69b4d62a1cb34e
EricB <ericblancas23@gmail.com>
2018-01-19 20:47:06 -0800

    few color changes

---
+++ src/Components/SlideOne.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SlideOne = (props) => {
+    let background = {
+        backgroundImage: url(''),
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+    }
+    return <div style={background} className="slide"></div>
+}
+
+export default SlideOne;
--- src/Components/navBar.js
+++ src/Components/navBar.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Button, ButtonGroup } from 'react-bootstrap';
+
+import '../App.css';
 
 export class NavBar extends Component {
   render() {
     return (
-      <div className="container row" id="header">
+      <div style={{backgroundColor: 'dodgerBlue'}} className="container-row" id="header">
         <header className="col-md-8">
           <div className="navigation">
             <div><img alt="logo" src={ require('../LambdaLogo.png') } /></div>
             <div className="hidden-sm-down"><h1>Lambda Showcase</h1></div>
-            <Link to='/signin'>Sign In</Link>
-            <Link to='/signup'>Sign Up</Link>
-            <Link to='/submit'>Submit</Link>
+              <Button className='navbuttons'><Link to='/signin'>Sign In</Link></Button>            
+              <Button className='navbuttons'><Link to='/signup'>Sign Up</Link></Button>
+              <Button className='navbuttons'><Link to='/submit'>Submit</Link></Button>
           </div>
         </header>
         
@@ -19,4 +22,3 @@ export class NavBar extends Component {
     );
   }
 }
-
