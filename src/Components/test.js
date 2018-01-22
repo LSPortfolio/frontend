9e814c70ca216617467c5151bd187713e742ae1f
Tyler Long <tyler@Tylers-MacBook-Pro.local>
2018-01-22 11:49:47 -0500

    working on sidebar

--- src/Components/test.js
+++ src/Components/test.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import ClassNavBar from './classNavBar';
-
+// import { DropdownButton, MenuItem } from 'react-bootstrap';
+import { Dropdown, Button } from 'bootstrap';
+import { DropdownButton } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 export default class Test extends Component {
     render () {
         return (
             <div>
                 <ClassNavBar />
+                <div class="btn-group dropleft">
+                    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropleft
+                    </button>
+                    <div class="dropdown-menu">
+                        <Link to='/'>1</Link>
+                        <Link to='/'>2</Link>
+                        <Link to='/'>3</Link>
+                        <Link to='/'>4</Link>
+                        <Link to='/'>5</Link>
+                    </div>
+                </div>
             </div>
         );
     }
