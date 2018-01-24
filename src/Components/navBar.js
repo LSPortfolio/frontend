6c4e93c3ef1026c0e69ac5daf3667365f903891b
Tyler Long <tyler@Tylers-MacBook-Pro.local>
2018-01-23 19:31:14 -0500

    refactored navbar

--- src/Components/navBar.js
+++ src/Components/navBar.js
@@ -16,19 +16,47 @@ export class NavBar extends Component {
                   <div style={{marginLeft: 5, marginRight: 10}}><img alt="logo" src={ require('../LambdaLogo.png') } /></div>
                   <div className="hidden-sm-down"><h1>Lambda Showcase</h1></div>
                 </div>
-                <div style={{marginRight: 25}}>
+                <div style={{marginRight: 65}}>
                   <Button className='navbuttons'><Link to='/signin'>Sign In</Link></Button>            
                   <Button className='navbuttons'><Link to='/signup'>Sign Up</Link></Button>
                   <Button className='navbuttons'><Link to='/submit'>Submit</Link></Button>
                   <Button className='navbuttons'><Link to='/Projects'>Student Projects</Link></Button>
-                  <DropdownButton id='dropdown' title="classes">
-                    <MenuItem id='cs1'>CS1</MenuItem>
-                    <MenuItem id='cs2'>CS2</MenuItem>
-                    <MenuItem id='cs3'>CS3</MenuItem>
-                    <MenuItem id='cs4'>CS4</MenuItem>
-                    <MenuItem id='cs5'>CS5</MenuItem>
-                    <MenuItem id='cs6'>CS6</MenuItem>
-                    <MenuItem id='cs7'>CS7</MenuItem>
+                  <DropdownButton id='dropdown' title="Classes">
+                    <DropdownButton className='graduated' title="CS1" id='cs1'>
+                      <MenuItem>Person 1</MenuItem>
+                      <MenuItem>Person 2</MenuItem>
+                      <MenuItem>Person 3</MenuItem>
+                    </DropdownButton>
+                    <DropdownButton title="CS2" id='cs2'>
+                      <MenuItem>Person 1</MenuItem>
+                      <MenuItem>Person 2</MenuItem>
+                      <MenuItem>Person 3</MenuItem>
+                    </DropdownButton>
+                    <DropdownButton title="CS3" id='cs3'>
+                      <MenuItem>Person 1</MenuItem>
+                      <MenuItem>Person 2</MenuItem>
+                      <MenuItem>Person 3</MenuItem>
+                    </DropdownButton>
+                    <DropdownButton title="CS4" id='cs4'>
+                      <MenuItem>Person 1</MenuItem>
+                      <MenuItem>Person 2</MenuItem>
+                      <MenuItem>Person 3</MenuItem>
+                    </DropdownButton>
+                    <DropdownButton title="CS5" id='cs5'>
+                      <MenuItem>Person 1</MenuItem>
+                      <MenuItem>Person 2</MenuItem>
+                      <MenuItem>Person 3</MenuItem>
+                    </DropdownButton>
+                    <DropdownButton title="CS6" id='cs6'>
+                      <MenuItem>Person 1</MenuItem>
+                      <MenuItem>Person 2</MenuItem>
+                      <MenuItem>Person 3</MenuItem>
+                    </DropdownButton>
+                    <DropdownButton title="CS7" id='cs7'>
+                      <MenuItem>Person 1</MenuItem>
+                      <MenuItem>Person 2</MenuItem>
+                      <MenuItem>Person 3</MenuItem>
+                    </DropdownButton>
                 </DropdownButton>
                 </div>
               </div>
