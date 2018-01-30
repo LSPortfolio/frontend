df3bd1343f7b15e4951d964b693c8c492e362b51
Tyler Long <tyler@Tylers-MacBook-Pro.local>
2018-01-30 12:27:42 -0500

    changes

--- lol.js
+++ lol.js
@@ -2,7 +2,7 @@
 
 const cloudinary = require('cloudinary');
 
-cloudinary.config({ cloud_name: 'dbquvargx', api_key: '236448318463957', api_secret: 'Cn4Gwl1iUM2nU4ogI3yFUcspxwc' });
+cloudinary.config({ cloud_name: '', api_key: '', api_secret: '' });
 
 cloudinary.uploader.upload(`https://wallpapershome.com/images/wallpapers/norway-3840x2160-5k-4k-wallpaper-fjord-mountains-river-sky-5657.jpg`, (err, result) => {
     console.log(result.url);
