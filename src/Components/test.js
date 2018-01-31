4b288079247c376067ac747db791299489539ab6
Tyler Long <tyler@Tylers-MacBook-Pro.local>
2018-01-31 17:10:52 -0500

    changes

--- src/Components/test.js
+++ src/Components/test.js
@@ -5,6 +5,7 @@ export default class Test extends Component {
     render () {
         return (
             <div style={{ display:'flex', flexDirection:'row' }}>
+                <h2>Share:</h2>
                 <div><img onClick={() => alert("STUFF")} className='medialogos' src={require('../Components/SocialMedia/facebookLogo.png')} /></div>
                 <div><img onClick={() => alert("STUFF")} className='medialogos' src={require('../Components/SocialMedia/instagramLogo.png')} /></div>
                 <div><img onClick={() => alert("STUFF")} className='medialogos' src={require('../Components/SocialMedia/linkedinLogo.png')} /></div>
