3ff6934c473a5c2b74d9e1edebda349572b59dd9
Tyler Long <tyler@Tylers-MacBook-Pro.local>
2018-01-31 20:40:35 -0500

    changes

--- src/Components/test.js
+++ src/Components/test.js
@@ -20,9 +20,9 @@ class ShowProjects extends Component {
     render() {
         return (
             <div style={{display: 'flex', flexDirection: 'column',}}>
-                <h2 style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>{ this.state.projects.one }</h2>
-                <img className='testimage' src={this.state.pics.one} />
-                <h4 style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>{ this.state.names.one }</h4>
+                <h2 style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>{ this.props.project }</h2>
+                <img className='testimage' src={this.props.pic} />
+                <h4 style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>{ this.props.name }</h4>
             </div>
         );
     }
@@ -33,42 +33,47 @@ export default class Test2 extends Component {
         super(props);
         this.state = {
             pics: {
-                one: require('../Components/TestFiles/1.jpg')
+                one: require('../Components/TestFiles/1.jpg'),
+                two: require('../Components/TestFiles/2.jpg'),
+                three: require('../Components/TestFiles/3.jpg'),
             },
             names: {
                 one: 'John Doe',
+                two: 'Jane Doe',
+                three: 'John Smith'
             },                
             projects: {
                 one: 'Project 1',
+                two: 'Project 2',
+                three: 'Project 3',
             }
         };
     }
 
     render() {
         return (
-            <div style={{display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap'}}>
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />
-                <ShowProjects />                
+            <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'wrap'}}>
+                <ShowProjects name={this.state.names.one} project={this.state.projects.one} pic={this.state.pics.one} />
+                <ShowProjects name={this.state.names.two} project={this.state.projects.two} pic={this.state.pics.two} />
+                <ShowProjects name={this.state.names.three} project={this.state.projects.three} pic={this.state.pics.three} />
+                <ShowProjects name={this.state.names.one} project={this.state.projects.one} pic={this.state.pics.one} />
+                <ShowProjects name={this.state.names.two} project={this.state.projects.two} pic={this.state.pics.two} />
+                <ShowProjects name={this.state.names.three} project={this.state.projects.three} pic={this.state.pics.three} />
+                <ShowProjects name={this.state.names.one} project={this.state.projects.one} pic={this.state.pics.one} />
+                <ShowProjects name={this.state.names.two} project={this.state.projects.two} pic={this.state.pics.two} />
+                <ShowProjects name={this.state.names.three} project={this.state.projects.three} pic={this.state.pics.three} />
+                <ShowProjects name={this.state.names.one} project={this.state.projects.one} pic={this.state.pics.one} />
+                <ShowProjects name={this.state.names.two} project={this.state.projects.two} pic={this.state.pics.two} />
+                <ShowProjects name={this.state.names.three} project={this.state.projects.three} pic={this.state.pics.three} />
+                <ShowProjects name={this.state.names.one} project={this.state.projects.one} pic={this.state.pics.one} />
+                <ShowProjects name={this.state.names.two} project={this.state.projects.two} pic={this.state.pics.two} />
+                <ShowProjects name={this.state.names.three} project={this.state.projects.three} pic={this.state.pics.three} />
+                <ShowProjects name={this.state.names.one} project={this.state.projects.one} pic={this.state.pics.one} />
+                <ShowProjects name={this.state.names.two} project={this.state.projects.two} pic={this.state.pics.two} />
+                <ShowProjects name={this.state.names.three} project={this.state.projects.three} pic={this.state.pics.three} />
+                <ShowProjects name={this.state.names.one} project={this.state.projects.one} pic={this.state.pics.one} />
+                <ShowProjects name={this.state.names.two} project={this.state.projects.two} pic={this.state.pics.two} />
+                <ShowProjects name={this.state.names.three} project={this.state.projects.three} pic={this.state.pics.three} />             
             </div>
 
         );
