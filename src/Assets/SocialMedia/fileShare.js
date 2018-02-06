ba8a0a555a7652db71cbea6a31618b8d66b84efa
Tyler Long <tyler@Tylers-MacBook-Pro.local>
2018-02-06 11:57:30 -0500

    finished basic functionality of linkedin/facebook/twitter/email share buttons

--- src/Assets/SocialMedia/fileShare.js
+++ src/Assets/SocialMedia/fileShare.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import '../../App.css';
 
 const linkedinLogo = require('../SocialMedia/linkedinLogo.png');
-const facebookLogo = require('../SocialMedia/facebookLogo.png');
+const facebookLogo = require('../SocialMedia/facebookLogo.com');
 const twitterLogo = require('../SocialMedia/twitterLogo.png');
 const instagramLogo = require('../SocialMedia/instagramLogo.png');
+const emailLogo = require('../SocialMedia/emailLogo.jpg');
 
 export class FileShare extends Component {
     render() {
@@ -29,14 +30,22 @@ export class FileShare extends Component {
                     }(document, 'script', 'facebook-jssdk'));
                 </script>
                 */}
-                <img className='logosize' src={ linkedinLogo } />
-                <img className='logosize' src={ facebookLogo } />
-                {/*
-                    Twitter: 
-                */}
+
+                
+                <a href="https://www.linkedin.com/shareArticle?mini=true&url=www.lambdaschool.com&title=qwerty&summary=&source=">
+                    <img className='logosize' src={ linkedinLogo } />
+                </a>
+                <a href="https://www.facebook.com/sharer/sharer.php?u=www.lambdaschool.com"><img className='logosize' src={ facebookLogo } /></a>
+                {/*Twitter*/}
                 <a class="twitter-share-button" href={`https://twitter.com/intent/tweet?` + `url=${url}&` + `via=LambdaSchool&text=Hello,%20World!&hashtags=LambdaSchool`} data-size="large" hashtags="lambda">
                     <img className='logosize' src={ twitterLogo } />
                 </a>
+                {/*Email*/}
+                <a href="mailto:?">
+                    <img className='logosize' src={ emailLogo } />
+                </a>
+                {/*Twitter:*/}
+                
             </div>
         );
     }
