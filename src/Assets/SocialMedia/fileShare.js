3d4e71fec12391ce264df073faa8c22302236082
Tyler Long <tyler@Tylers-MacBook-Pro.local>
2018-02-06 13:45:23 -0500

    fileShare.js

--- src/Assets/SocialMedia/fileShare.js
+++ src/Assets/SocialMedia/fileShare.js
@@ -11,21 +11,26 @@ export class FileShare extends Component {
     render() {
         const url = 'www.lambdaschool.com';
         return (
-            <div className='logoholder'>                
-                <a href="https://www.linkedin.com/shareArticle?mini=true&url=www.lambdaschool.com&title=qwerty&summary=&source=">
-                    <img className='logosize' src={ linkedinLogo } />
-                </a>
-                <a href="https://www.facebook.com/sharer/sharer.php?u=www.lambdaschool.com"><img className='logosize' src={ facebookLogo } /></a>
-                {/*Twitter*/}
-                <a class="twitter-share-button" href={`https://twitter.com/intent/tweet?` + `url=${url}&` + `via=LambdaSchool&text=Hello,%20World!&hashtags=LambdaSchool`} data-size="large" hashtags="lambda">
-                    <img className='logosize' src={ twitterLogo } />
-                </a>
-                {/*Email*/}
-                <a href="mailto:?">
-                    <img className='logosize' src={ emailLogo } />
-                </a>
-                {/*Twitter:*/}
-                
+            <div>
+                <p>Share this project:</p>
+                <div className='logoholder'>
+                    {/*LinkedIn*/}
+                    <a href="https://www.linkedin.com/shareArticle?mini=true&url=www.lambdaschool.com&title=qwerty&summary=&source=">
+                        <img className='logosize' src={ linkedinLogo } />
+                    </a>
+                    {/*Facebook*/}
+                    <a href="https://www.facebook.com/sharer/sharer.php?u=www.lambdaschool.com">
+                        <img className='logosize' src={ facebookLogo } />
+                    </a>
+                    {/*Twitter*/}
+                    <a class="twitter-share-button" href={`https://twitter.com/intent/tweet?` + `url=${url}&` + `via=LambdaSchool&text=Hello,%20World!&hashtags=LambdaSchool`} data-size="large" hashtags="lambda">
+                        <img className='logosize' src={ twitterLogo } />
+                    </a>
+                    {/*Email*/}
+                    <a href="mailto:?">
+                        <img className='logosize' src={ emailLogo } />
+                    </a>
+                </div>
             </div>
         );
     }
