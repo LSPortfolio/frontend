b8370408125ff59b4177ebbed70d19c9fc69420f
Tyler Long <tyler@Tylers-MacBook-Pro.local>
2018-02-07 14:00:37 -0500

    changes

---
+++ src/Assets/SocialMedia/modals/linkedinModal.js
@@ -0,0 +1,10 @@
+// import React, { Component } from 'react';
+// import Modal from 'react-modal';
+
+// export default class LinkedinModal extends Component () {
+//     render() {
+//         return (
+//             <div>LINKEDIN</div>
+//         );
+//     }
+// }
