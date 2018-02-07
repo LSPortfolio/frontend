3725ce3ded96733cf3e217cfa80bec7a387e8557
EricB <ericblancas23@gmail.com>
2018-02-07 10:55:59 -0800

    homepage finished and styled

---
+++ src/Components/projectDisplay.js
@@ -0,0 +1,12 @@
+// import React, { Component } from 'react';
+
+
+// export default class ProjectDisplay {
+//     constructor() {
+//         super();
+//         this.state = {
+//             image: '',
+//             contributor: '',
+//         }
+//     }
+// }
