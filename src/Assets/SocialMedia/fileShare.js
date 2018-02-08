7b9af536dc1e7558593c69839036e9bfed71a785
Tyler Long <tyler@Tylers-MacBook-Pro.local>
2018-02-08 15:51:34 -0500

    All social media components now functional. Facebook not functional on localhost.

--- src/Assets/SocialMedia/fileShare.js
+++ src/Assets/SocialMedia/fileShare.js
@@ -1,114 +1,101 @@
 import React, { Component } from 'react';
 import '../../App.css';
+import { FormGroup, FormControl, ButtonToolbar } from 'react-bootstrap';
 import Modal from 'react-modal';
-import FacebookModal from '../SocialMedia/modals/facebookModal';
-import { FormGroup, FormControl, Button, ButtonToolbar } from 'react-bootstrap';
-
-
 
 const linkedinLogo = require('../SocialMedia/linkedinLogo.png');
 const facebookLogo = require('../SocialMedia/facebookLogo.com');
 const twitterLogo = require('../SocialMedia/twitterLogo.png');
 const emailLogo = require('../SocialMedia/emailLogo.jpg');
 
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
 export class FileShare extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            linkedinToggleVisible: false,
-            facebookToggleVisible: false,
-            twitterToggleVisible: false,
             emailToggleVisible: false,
-            title: ''
-
+            title: 'Lambda School',
+            url: 'www.lambdaschool.com',
+            source: 'Lambda School',
+            summary: 'A testing route',
+            text: '',
+            emailrecipient: '',
+            emailCC: '',
+            emailBCC: '',
+            emailSubject: '',
+            emailBody: '',
         };
     }
 
     render() {
-        const url = 'www.lambdaschool.com';
+        const joinWords = (oldDescription) => {
+            let newDescription = oldDescription.split(' ').join('%20');
+            return newDescription;
+        }
         return (
             <div className='logoholder'>
-                
-                <Modal className='modalstyles' isOpen={this.state.linkedinToggleVisible}>
-                    <h2>Share on LinkedIn:</h2>
-                    <div style={{ marginTop: 30 }}>
-                        <FormGroup>
-                            <FormControl
-                                type='text'
-                                placeholder='Title'
-                                value={this.state.title}
-                                onChange={(e) => this.setState({ title: e.target.value })}
-                            />
-                            <FormControl
-                                type='text'
-                                placeholder='Title'
-                                value={this.state.title}
-                                onChange={(e) => this.setState({ title: e.target.value })}
-                            />
-                        </FormGroup>
-
-                        <div className='mediabuttons'>
-                            <Button className='mediasharebuttonlinkedin'>Share</Button>
-                            <Button onClick={()=>this.setState({linkedinToggleVisible: !this.state.linkedinToggleVisible})} className='mediaclosebutton'>Close</Button>
-                        </div>
-                    </div>
-                    
-                </Modal>
-
-                <Modal className='modalstyles' isOpen={this.state.facebookToggleVisible}>
-                    <h2>Share on Facebook:</h2>
-                    <div className='mediabuttons'>
-                        <Button className='mediasharebuttonfacebook'>Share</Button>
-                        <Button onClick={()=>this.setState({facebookToggleVisible: !this.state.facebookToggleVisible})} className='mediaclosebutton'>Close</Button>
-                    </div>
-                </Modal>
-
-                <Modal className='modalstyles' isOpen={this.state.twitterToggleVisible}>
-                    <h2>Share on Twitter:</h2>
-                    <div className='mediabuttons'>
-                        <Button className='mediasharebuttontwitter'>Share</Button>
-                        <Button onClick={()=>this.setState({twitterToggleVisible: !this.state.twitterToggleVisible})} className='mediaclosebutton'>Close</Button>
-                    </div>
-                </Modal>
-
-                <Modal className='modalstyles' isOpen={this.state.emailToggleVisible}>
-                    <h2>Share by email:</h2>
-                    <div className='mediabuttons'>
-                        <Button className='mediasharebuttonemail'>Share</Button>
-                        <Button onClick={()=>this.setState({emailToggleVisible: !this.state.emailToggleVisible})} className='mediaclosebutton'>Close</Button>
+                <Modal isOpen={this.state.emailToggleVisible}>
+                    <h1>Send an Email:</h1>
+                    <h6>*Separate multiple recipients/CC/BCC with a semicolon</h6>
+                    <FormGroup>
+                        <FormControl
+                            type='text'
+                            placeholder='To:'
+                            onChange={(e) => this.setState({ emailrecipient: e.target.value })}
+                        />
+                        <FormControl
+                            type='text'
+                            placeholder='Subject:'
+                            onChange={(e) => this.setState({ emailSubject: e.target.value })}
+                        />
+                        <FormControl
+                            type='text'
+                            placeholder='CC:'
+                            onChange={(e) => this.setState({ emailCC: e.target.value })}
+                        />
+                        <FormControl
+                            type='text'
+                            placeholder='BCC:'
+                            onChange={(e) => this.setState({ emailBCC: e.target.value })}
+                        />
+                        <textarea
+                            className='form-group'
+                            type='textarea'
+                            rows='8'
+                            style={{width:'100%'}}
+                            placeholder='Email Body:'
+                            onChange={(e) => this.setState({ emailBody: e.target.value })}
+                        />
+                    </FormGroup>
+                    <div>
+                        <button>
+                            <a onClick={() => this.setState({ emailToggleVisible: !this.state.emailToggleVisible })} href={`mailto:${this.state.emailrecipient.split(' ').join()}?&cc=${this.state.emailCC.split(' ').join()}&subject=${this.state.emailSubject.split(' ').join('%20')}&bcc=${this.state.emailBCC.split(' ').join()}&body=${this.state.emailBody.split(' ').join('%20')}`}>
+                                Send
+                            </a>
+                        </button>
+                        
+                        <button onClick={() => this.setState({ emailToggleVisible: !this.state.emailToggleVisible })}>
+                            Cancel
+                        </button>
                     </div>
                 </Modal>
-
-                <img onClick={()=>this.setState({linkedinToggleVisible: !this.state.linkedinToggleVisible})} className='logosize imagesmadeformediabuttons' src={require('../SocialMedia/linkedinLogo.png')} />
-                <img onClick={()=>this.setState({facebookToggleVisible: !this.state.facebookToggleVisible})} className='logosize imagesmadeformediabuttons' src={require('../SocialMedia/facebookLogo.com')} />
-                <img onClick={()=>this.setState({twitterToggleVisible: !this.state.twitterToggleVisible})} className='logosize imagesmadeformediabuttons' src={require('../SocialMedia/twitterLogo.png')} />
-                <img onClick={()=>this.setState({emailToggleVisible: !this.state.emailToggleVisible})} className='logosize imagesmadeformediabuttons' src={require('../SocialMedia/emailLogo.jpg')} />
-
-
-
-
-
-
-
-
-
-
-
                 {/*LinkedIn*/}
-                <a href="https://www.linkedin.com/shareArticle?mini=true&url=www.lambdaschool.com&title=qwerty&summary=&source=">
-                    linkedin
+                <a href={`https://www.linkedin.com/shareArticle?mini=true&url=${window.location.href}&title=${joinWords(this.state.title)}&summary=${joinWords(this.state.summary)}&source=${joinWords(this.state.source)}`} target="_blank">
+                    <img className='logosize imagesmadeformediabuttons' src={linkedinLogo} />
                 </a>
                 {/*Facebook*/}
-                <a href="https://www.facebook.com/sharer/sharer.php?u=www.lambdaschool.com">
-                    facebook
+                <a href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}`} target="_blank">
+                    <img className='logosize imagesmadeformediabuttons' src={facebookLogo} />
                 </a>
                 {/*Twitter*/}
-                <a class="twitter-share-button" href={`https://twitter.com/intent/tweet?` + `url=${url}&` + `via=LambdaSchool&text=Hello,%20World!&hashtags=LambdaSchool`} data-size="large" hashtags="lambda">
-                    twitter
+                <a className="twitter-share-button" href={`https://twitter.com/intent/tweet?url=${window.location.href}&via=LambdaSchool&text=${joinWords(this.state.text)}&hashtags=LambdaSchool`} target="_blank">
+                    <img className='logosize imagesmadeformediabuttons' src={twitterLogo} />
                 </a>
                 {/*Email*/}
-                <a href="mailto:?">
-                    email
+                <a onClick={() => this.setState({ emailToggleVisible: !this.state.emailToggleVisible })}>
+                    <img className='logosize imagesmadeformediabuttons' src={emailLogo}  target="_blank"/>
                 </a>
 
             </div>
