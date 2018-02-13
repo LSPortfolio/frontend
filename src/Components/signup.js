572109fc9923297d6da903ba03f0610af6a7bfff
Tyler Long <tyler@Tylers-MacBook-Pro.local>
2018-02-13 13:29:59 -0500

    changes

--- src/Components/signup.js
+++ src/Components/signup.js
@@ -4,6 +4,18 @@ import { FormControl, FormGroup } from 'react-bootstrap'
 
 import { register } from '../Actions'
 
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth20').Strategy;
+// passport.use(
+//   new GoogleStrategy({
+//     callbackURL: '/auth/google/redirect',
+//     clientID: process.env.GOOGLE_CLIENT_ID,
+//     clientSecret: process.env.GOOGLE_CLIENT_SECRET
+//   }), () => {
+
+//   }
+// );
+
 class SignUp extends Component {
   constructor() {
     super()
